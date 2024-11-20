7fd1d4c0dc4a8c5ab3d90eb42646d41b02c391ac
ClickPaw <corlecoz@gmail.com>
2024-11-20 16:48:52 +0800

    Up

--- dist/index.cjs.js
+++ dist/index.cjs.js
@@ -2429,7 +2429,7 @@ var ConnectModal = function (_a) {
     var login = _a.login, _b = _a.onDismiss, onDismiss = _b === void 0 ? function () { return null; } : _b;
     return (React__default['default'].createElement(Modal, { title: "Connect to a wallet", onDismiss: onDismiss },
         connectors.map(function (entry, index) { return (React__default['default'].createElement(WalletCard, { key: entry.title, login: login, walletConfig: entry, onDismiss: onDismiss, mb: index < connectors.length - 1 ? "8px" : "0" })); }),
-        React__default['default'].createElement(HelpLink, { href: "https://docs.pancakeswap.finance/guides/faq#how-do-i-set-up-my-wallet-on-binance-smart-chain", external: true },
+        React__default['default'].createElement(HelpLink, { href: "", external: true },
             React__default['default'].createElement(Icon$o, { color: "text", mr: "6px" }),
             "Learn how to connect")));
 };
